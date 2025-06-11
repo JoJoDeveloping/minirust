1f41b890d4fd6fc664ac1726ea8de8484df27b2c
Xinglu Chen <public@yoctocell.xyz>
2025-06-11 16:04:00 +0200

    Copy cell_inside_struct test from Miri

---
+++ tooling/minimize/tests/ub/tree_borrows/core/cell_inside_struct.rs
@@ -0,0 +1,24 @@
+//@ compile-flags: --minimize-tree-borrows
+
+use std::cell::Cell;
+
+struct Foo {
+    field1: u32,
+    field2: Cell<u32>,
+}
+
+pub fn main() {
+    let root = Foo { field1: 42, field2: Cell::new(88) };
+    // let root = (42, Cell::new(88));
+    unsafe {
+        let a = &root;
+        let a: *mut Foo = a as *const _ as *mut _;
+        // let a: *mut (i32, Cell<i32>) = a as *const _ as *mut _;
+
+        // Writing to `field2`, which is interior mutable, should be allowed.
+        (*a).field2.set(10);
+
+        // Writing to `field1`, which is frozen, should not be allowed.
+        (*a).field1 = 88; // UB! Child write to frozen.
+    }
+}
